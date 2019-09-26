b07eab0951caec2bf2f9b72cafc32d35b0eb5e42
ming0601 <fangming0601@gmail.com>
2019-09-26 22:52:06 +0200

    Updated MapsTest with Object test

--- src/test/kotlin/extensionMethods/MapsTest.kt
+++ src/test/kotlin/extensionMethods/MapsTest.kt
@@ -18,10 +18,19 @@ class MapsTest {
         val output = listOf("New York", "Paris")
         val output1 = listOf("John", "Jean")
         val output2 = listOf(30, 40)
+        val output3 = listOf(
+            Address(number = 10, streetName = "Washington Road", postalCode = 300100, cityName = "New York"),
+            Address(number = 100, streetName = "Boulevard de la République", postalCode = 75011, cityName = "Paris")
+        )
 
+        // Working on String
         Assertions.assertEquals(output, inputCustomer.mapAny { it.address.cityName })
         Assertions.assertEquals(output1, inputCustomer.mapAny { it.name })
+        // Working on Int
         Assertions.assertEquals(output2, inputCustomer.mapAny { it.age })
+        // Checking that we have the same result with the standard .map {}
         Assertions.assertEquals(output2, inputCustomer.map { it.age })
+        // Working on Object
+        Assertions.assertEquals(output3, inputCustomer.map { it.address })
     }
 }
